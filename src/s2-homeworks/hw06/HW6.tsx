3fae79ee293f9f971e96c9d561070500fd1472bd
Talman95 <taltro528@gmail.com>
2023-04-13 11:44:41 +0300

    added hw6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,8 +18,8 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        // делают студенты
-
+        const stateFromLocalStorage = restoreState<string>('hw6-editable-span-value', value)
+        setValue(stateFromLocalStorage)
     }
 
     return (
