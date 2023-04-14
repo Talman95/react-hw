e585649fda2d57e21ba5c7b3a21a5c20d2495852
Talman95 <taltro528@gmail.com>
2023-04-14 11:18:20 +0300

    corrected hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,14 +4,14 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': {
-            if (action.payload === 'up') {
-                return state.sort((a, b) => a.name.localeCompare(b.name))
-            } else {
-                return state.sort((a, b) => b.name.localeCompare(a.name))
-            }
+            return action.payload === 'up'
+                ?
+                [...state].sort((a, b) => a.name.localeCompare(b.name))
+                :
+                [...state].sort((a, b) => b.name.localeCompare(a.name))
         }
         case 'check': {
             return state.filter(({age}) => age >= 18)
